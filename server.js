955649ae601b635a49f3b7a49e1a9cfec6570dd7
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-20 16:53:38 -0800

    ADDED: helpful comments

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+// npm for .env folder which hides our passwords
 require("dotenv").config();
+
+// npm for routes
 const express = require("express");
 const PORT = process.env.PORT || 8080;
+
+// set up app as a instance of express
 const app = express();
+// able to parse through string and json data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// uses public folder for html and css
 app.use(express.static("public"));
 
+// open port upon starting app
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
