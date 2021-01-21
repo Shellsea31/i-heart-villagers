789907f9c8e91051a8af556aac40f6bcf2f05617
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-20 16:50:59 -0800

    ADDED: required npms in server.js, setup for json ans string format, open port on start

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
