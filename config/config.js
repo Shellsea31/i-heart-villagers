1781b90cb921e6de3fea1742a31147153e4d86ba
TruKat <kati.k220@gmail.com>
2021-01-25 20:05:07 -0700

    modifying main.html

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports =
 {
   "development": {
     "username": "root",
-    "password": process.env.SDB_PASS,
+    "password": process.env.trukatDB,
     "database": "userLogin",
     "host": "127.0.0.1",
     "dialect": "mysql"
