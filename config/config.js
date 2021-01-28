b1eb9617d40588b707552b1bc39d39396bc63a33
Shelsy Elaine <elainesbarrera@gmail.com>
2021-01-28 14:04:58 -0800

    changes config again

--- config/config.js
+++ config/config.js
@@ -1,22 +1,23 @@
 require("dotenv").config();
 
-module.exports = {
-  development: {
-    username: "root",
-    password: process.env.SDB_PASS,
-    database: "userLogin",
-    host: "127.0.0.1",
-    dialect: "mysql",
+module.exports =
+{
+  "development": {
+    "username": "root",
+    "password": process.env.SDB_PASS,
+    "database": "userLogin",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   },
-  production: {
-    use_env_variable: process.env.JAWSDB_URL,
-    dialect: "mysql",
-  },
-};
+  "production": {
+    "use_env_variable":"JAWSDB_URL",
+    "dialect": "mysql"
+  }
+}
